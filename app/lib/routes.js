903c7a76321c70e5455bb09915befa4a6ae7693e
Paul Grever <paulgrever@gmail.com>
2015-09-22 18:06:17 -0600

    finished section 4 Routes and Controllers

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -1,6 +1,11 @@
-
+Router.configure({
+  layoutTemplate: "MasterLayout",
+  loadingTemplate: "Loading",
+  notFoundTemplate: "NotFound"
+});
 Router.route('/', {
   name: 'home',
   controller: 'HomeController',
+  action: 'action',
   where: 'client'
 });
